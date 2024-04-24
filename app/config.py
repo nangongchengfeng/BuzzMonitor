3a4a7b398c1cf00152ca4fdb4370b99b1d1702e0
南宫乘风 <1794748404@qq.com>
2024-04-24 11:34:26 +0800

    增加:框架机构化搭建

---
+++ app/config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024-04-24 11:33
+# @Author  : 南宫乘风
+# @Email   : 1794748404@qq.com
+# @File    : config.py.py
+# @Software: PyCharm
---
+++ manage.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024-04-24 11:25
+# @Author  : 南宫乘风
+# @Email   : 1794748404@qq.com
+# @File    : app.py
+# @Software: PyCharm
